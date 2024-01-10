23894fb0f52488e9783d3347526c50ca4e2c78cf
YTDavid93 <ytd3809@gmail.com>
2024-01-10 16:11:25 +0545

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import styled from "styled-components"
+import { breakpoints } from "./constants/BreakPoints"
+
+const Grid = styled.div `
+  display: grid;
+  grid-template-areas: 
+    "nav nav" 
+    "aside main";
+
+  ${breakpoints.small} {
+    grid-template-areas: 
+      "nav nav"
+      "main main";
+  }
+`
+const Nav = styled.nav `
+  background-color: red;
+  grid-area: nav;
+`
+
+const Aside = styled.aside`
+  background-color: blue;
+  grid-area: aside;
+
+  ${breakpoints.small} {
+    display: none;
+  }
+`
+const Main = styled.main `
+  background-color: green;
+  grid-area: main;
+`
 
 const App = () => {
-  return 
-};
+  return (
+      <Grid>
+        <Nav>Nav</Nav>
+        <Aside>aside</Aside>
+        <Main>Main</Main>
+      </Grid>
+  )
+}
 
-export default App;
- 
+export default App
