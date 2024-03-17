944f7d643b974a7eca7bab7f04c3e42bef0e94b1
YTDavid93 <ytd3809@gmail.com>
2024-03-17 15:55:50 +0545

    Build search button

--- src/components/SearchInputs.tsx
+++ src/components/SearchInputs.tsx
@@ -1,6 +1,7 @@
 import { useRef } from "react";
 import styled from "styled-components";
-
+import { BsSearch } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
 
 const InputItem = styled.input`
   width: 100%;
@@ -11,26 +12,45 @@ const InputItem = styled.input`
   background-color: #3f3f46;
   color: #fff;
   font-size: 1rem;
+  padding: 28px;
 
   &:focus {
     outline: none;
   }
 `;
 
+const Div = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const inconStyle = {
+  position: "absolute",
+  top: "18px",
+  left: "5px",
+  width: "15px",
+  cursor: "Pointer",
+};
+
 interface Props {
-    onSearch: (searchText: string) => void;
+  onSearch: (searchText: string) => void;
 }
 
+
 const SearchInputs = ({ onSearch }: Props) => {
-  const ref = useRef<HTMLInputElement>(null)
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-   <form onSubmit={(e) => {
-      e.preventDefault();
-      if(ref.current) onSearch(ref.current.value)
-    }}>
-    <InputItem ref={ref} type="text" placeholder="Search games..." />  
-   </form> 
-  )
-}
+    <form onSubmit={
+      (e) => {e.preventDefault()
+      if(ref.current) onSearch(ref.current.value) }
+    }>
+      <Div>
+        <BsSearch style={inconStyle} />
+        <InputItem ref={ref} type="text" placeholder="Search games..." />
+      </Div>
+    </form>
+  );
+};
 
 export default SearchInputs;
